c56d64515282a8e46c3a3a804124726aa897dcf6
TosAlert <saidmurodovsuxrob288@gmail.com>
2026-07-15 18:23:00 +0500

    Update chart.py

--- services/chart.py
+++ services/chart.py
@@ -150,7 +150,7 @@ class ChartDownloader:
             page.goto(url, wait_until="commit", timeout=30000)
 
         page.set_viewport_size({
-            "width": 1600,
+            "width": 700,
             "height": 1600,
         })
 
